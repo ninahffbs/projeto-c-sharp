afd8ce13186e885f9890c63b90e41403a16ccc38
ninahffbs <nina.hffs@gmail.com>
2026-01-12 20:18:23 -0300

    programa tabuada

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int j = 1;
+		while(j <= 10){
+			for(int i = 1; i <= 10; i++){
+				Console.WriteLine($"{j} x {i} = {j * i}");
+			}
+			Console.WriteLine("\n");
+			j+= 1;
+		}
+    }
+}
